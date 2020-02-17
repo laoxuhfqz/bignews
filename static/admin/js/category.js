b1c4c656c732630c39a9aa00a7cd4c85a8537222
niz <1984088198@qq.com>
2020-02-17 20:46:03 +0800

    倪忠分类管理第一次上传

---
+++ static/admin/js/category.js
@@ -0,0 +1,71 @@
+$.ajax({
+    type: 'get',
+    url: 'http://localhost:8080/api/v1/admin/category/list',
+    success: function (response) {
+        if (response.code == 200) {
+            var html = template('categorytpl', response);
+            $('#categorybox').html(html);
+
+        }
+    }
+})
+
+//新增分类
+$('#model_add').on('click', function () {
+    var data = $('#addcategory').serialize();
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/category/add',
+        type: 'post',
+        data: data,
+        success: function (result) {
+            location.reload()
+        }
+    })
+
+})
+
+$('#categorybox').on('click', '.edit', function () {
+    var id = $(this).data('id');
+
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/admin/category/search',
+        data: { id },
+        success: function (result) {
+            $('#addModal').modal('show');
+            var html = template('modifytpl', result);
+            $('#modifybox').html(html);
+        }
+    })
+})
+
+$('#modifybox').on('click', '#model_modify', function () {
+    var data = $('#modifycategory').serialize();
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/category/edit',
+        type: 'post',
+        data: data,
+        success: function (result) {
+            if (result.code == 200) {
+                location.reload()
+            }
+        }
+    })
+})
+
+$('#addModal').on('click', '#shutoff', function () {
+    $('#addModal').modal('hide')
+})
+
+
+$('#categorybox').on('click', '.delete', function () {
+    var id = $(this).siblings('.edit').data('id');
+    $.ajax({
+        url: 'http://localhost:8080/api/v1/admin/category/delete',
+        type: 'post',
+        data: {id},
+        success: function (response) {
+            location.reload();
+        }
+    })
+})
